0c5a21b7b6b86d0faf1e4ca827fdc22aaf144b00
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2014-09-16 23:00:13 -0400

    Test cases Running as expected with a Valgring Error

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -1,34 +1,53 @@
-
 #include"answer03.h"
 
 
 char * strcat_ex(char** dest, int * n, const char * src)
 {
-	int min_length = strlen(*dest) + strlen(src) + 1;
 
-	if((*n == 0) ||(*n < (min_length)))
-	{	
-		char* new_dest;
-		new_dest = (char*)malloc(sizeof(char)*(1 + 2*(strlen(*dest) + strlen(src))));
-		*n = (1 + 2*(strlen(*dest) + strlen(src)));
-		strcpy(new_dest,(char*)*dest);
-		free(*dest);
-		*dest = new_dest; // memcpy(*dest,new_dest,*n)
-		strcpy((char*)*dest,src);
-		//free(new_dest);
+	char* new_dest_string;
+	
+	/*if(src == NULL && *dest == NULL)
+	{
+		return(" ");
+	}*/
+
+	
+	if((*dest == NULL) ||( *n < (1 + strlen(src) + strlen(*dest))))
+	{			
+		if(*dest == NULL)
+		{
+			new_dest_string = malloc(sizeof(char)*(1 + 2*(strlen(src))));
+			*n = (1 + 2*(strlen(src)));
+			 //strcpy(new_dest_string,*dest); // This doesn't work because *dest is not a string
+			 // free(*dest); // Is freeing an Invalid Pointer
+		}	
+		
+		else
+		{		
+			new_dest_string = malloc(sizeof(char)*(1 + 2*(strlen(*dest) + strlen(src))));
+			*n = (1 + 2*(strlen(*dest) + strlen(src)));
+			strcpy(new_dest_string,*dest);
+			free(*dest);
+
+		}				
+		
+		*dest = new_dest_string; 
+		strcat(*dest,src);
+		//free(new_dest_string); // Doing this Will return an Empty String instead use free(dest) in the main function
 	}
 
 	else
 	{
-		strcpy((char*)*dest,src);
+		strcat(*dest,src);
 	}
+	
+	
 
-	return(char*)(*dest);
+	return(*dest);
 }
 
 
-//char * * explode(const char * str, const char * delims, int * arrLen)
-//{
+		
 		
 
 
