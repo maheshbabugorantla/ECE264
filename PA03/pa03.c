c9682576e69bc7558a7d5a26c65937b421678111
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2014-09-16 23:04:49 -0400

    Updated the Test Cases for 'strcat_ex' function

--- PA03/pa03.c
+++ PA03/pa03.c
@@ -1,25 +1,56 @@
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
 #include"answer03.h"
 
-#define MEMORY 25
-
 int main(int argc, char** argv)
 {
-	//const int size = 25;
-	char* dest = "Mahesh";
-	//char** dest1 = &dest;
-	int size = strlen(dest);
-	char* src = "Babu";
+    // Test Case 1 for strcat_ex
+	int n = 10;
+	char* dest;
+	dest = malloc(sizeof(char)*n);
+	strcpy(dest,"Mahesh");
+	const char* src = " Babu";	
+	printf("The String1 is %s\n\n",strcat_ex(&dest,&n,src));
 
-	printf("The String is %s \n\n",strcat_ex(&dest,&size,src));
+	free(dest);
 
+    // Test Case 2 for strcat_ex
+
+    	n = 0; // Because of NULL Pointer
+	dest = NULL;
+	src = "Mahesh Babu Gorantla";
+
+	printf("The String2 is %s\n\n",strcat_ex(&dest,&n,src));
+	free(dest); // Memory is allocated in the Function in this Case. :)
+
+    // Test Case 3 for strcat_ex
+
+	n = 12;
+
+	dest = malloc(sizeof(char)*n);
+	strcpy(dest,"Mahesh Babu");
+
+	src = " Gorantla";
+
+	printf("The String3 is %s\n\n",strcat_ex(&dest,&n,src));
+	free(dest); // Memory is allocated in the Function in this Case. :)
+
+
+     /*// Test Case 4 for strcat_ex
+
+	n = 0;
+	dest = NULL;
+	src = NULL;
+
+	printf("The String4 is %s\n\n",strcat_ex(&dest,&n,src));
+	//free(dest); // Memory is allocated in the Function in this Case. :)
+	*/			
+				
 	return(EXIT_SUCCESS);
 }
+
 
 
 
