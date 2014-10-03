8a0fa64cb8cb4f04baa39bdecb57ab9e018ee3f8
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2014-10-03 19:21:26 -0400

    Functions Working as Expected but not complete

--- PA04/Part2/cat-lite.c
+++ PA04/Part2/cat-lite.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 
 int main(int argc , char** argv)
 {
@@ -8,6 +10,19 @@ int main(int argc , char** argv)
 	int index = 0; // Used to run through all the Arguments in the Command Line
 
 	int c;
+
+
+	for(index = 1;index < argc;index++)
+	{
+		if(strcmp(argv[index],"--help") == 0)
+		{
+			printf("Usage: cat-lite [--help] [FILE]...\nWith no FILE, or when FILE is -, read standard input.\n\nExamples:\n  cat-lite README   Print the file README to standard output.\n  cat-lite f - g    Print f's contents, then standard input, \n                    then g's contents.\n  cat-lite          Copy standard input to standard output.\n\n"); 
+
+			return(EXIT_SUCCESS);
+		}
+	}
+			
+	index = 0;
 
 	for(index = 1; index < argc; index++)
 	{
